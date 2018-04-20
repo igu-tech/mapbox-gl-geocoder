4baf26bdecfcbe2d2afaebb4be816eb9d5c0f575
tobinbradley <tobin.bradley@gmail.com>
2018-04-20 13:31:20 -0400

    patched for Mecklenburg's geocoder

--- README.md
+++ README.md
@@ -1,35 +1,11 @@
-Mapbox GL Geocoder [![CircleCI](https://circleci.com/gh/mapbox/mapbox-gl-geocoder.svg?style=svg)](https://circleci.com/gh/mapbox/mapbox-gl-geocoder)
----
+## Mapbox GL Geocoder [![CircleCI](https://circleci.com/gh/mapbox/mapbox-gl-geocoder.svg?style=svg)](https://circleci.com/gh/mapbox/mapbox-gl-geocoder)
 
-A geocoder control for [mapbox-gl-js](https://github.com/mapbox/mapbox-gl-js) using the [Mapbox Geocoding API](https://www.mapbox.com/api-documentation/#geocoding).
+This control is customized to use Mecklenburg County's geocoding API rather than Mapbox's, as well as fetch/promise polyfills.
 
-### Usage
+I added a `searchTypes` option to specify the search types for our API. Default is "address".
 
-https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder/
-
-### Usage with a module bundler
-
-This module exports a single class called MapboxGeocoder as its default export,
-so in browserify or webpack, you can require it like:
-
-```js
-var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
+```javascript
+var geocoder = new MapboxGeocoder({
+  searchTypes: ["address", "park"]
+});
 ```
-
-### Deeper dive
-
-#### API Documentation
-
-See [API.md](https://github.com/mapbox/mapbox-gl-geocoder/blob/master/API.md) for complete reference.
-
-#### Examples
-
- - [Add a geocoder to Mapbox GL JS](https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder/)
- - [Place the geocoder input outside the map](https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder-outside-the-map/)
- - [Limit geocoder results to a named region](https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder-limit-region/)
- - [Supplement geocoding search results from another data source](https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder-local-geocoder)
- - [Bias geocoder results around the map view](https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder-proximity-bias)
-
-### Contributing
-
-See [CONTRIBUTING.md](https://github.com/mapbox/mapbox-gl-geocoder/blob/master/CONTRIBUTING.md).
--- debug/index.js
+++ debug/index.js
@@ -1,21 +1,23 @@
-'use strict';
-var mapboxgl = require('mapbox-gl');
-var insertCss = require('insert-css');
-var fs = require('fs');
-mapboxgl.accessToken = window.localStorage.getItem('MapboxAccessToken');
+"use strict";
+var mapboxgl = require("mapbox-gl");
+var insertCss = require("insert-css");
+var fs = require("fs");
+mapboxgl.accessToken = window.localStorage.getItem("MapboxAccessToken");
 
-var meta = document.createElement('meta');
-meta.name = 'viewport';
-meta.content = 'initial-scale=1,maximum-scale=1,user-scalable=no';
-document.getElementsByTagName('head')[0].appendChild(meta);
+var meta = document.createElement("meta");
+meta.name = "viewport";
+meta.content = "initial-scale=1,maximum-scale=1,user-scalable=no";
+document.getElementsByTagName("head")[0].appendChild(meta);
 
-insertCss(fs.readFileSync('./lib/mapbox-gl-geocoder.css', 'utf8'));
-insertCss(fs.readFileSync('./node_modules/mapbox-gl/dist/mapbox-gl.css', 'utf8'));
+insertCss(fs.readFileSync("./lib/mapbox-gl-geocoder.css", "utf8"));
+insertCss(
+  fs.readFileSync("./node_modules/mapbox-gl/dist/mapbox-gl.css", "utf8")
+);
 
-var MapboxGeocoder = require('../');
+var MapboxGeocoder = require("../");
 
-var mapDiv = document.body.appendChild(document.createElement('div'));
-mapDiv.style.position = 'absolute';
+var mapDiv = document.body.appendChild(document.createElement("div"));
+mapDiv.style.position = "absolute";
 mapDiv.style.top = 0;
 mapDiv.style.right = 0;
 mapDiv.style.left = 0;
@@ -23,86 +25,47 @@ mapDiv.style.bottom = 0;
 
 var map = new mapboxgl.Map({
   container: mapDiv,
-  style: 'mapbox://styles/mapbox/streets-v9',
+  style: "mapbox://styles/mapbox/streets-v9",
   center: [-79.4512, 43.6568],
   zoom: 13
 });
 
-var coordinatesGeocoder = function (query) {
-    var matches = query.match(/^[ ]*(-?\d+\.?\d*)[, ]+(-?\d+\.?\d*)[ ]*$/);
-    if (!matches) {
-        return null;
-    }
-    function coordinateFeature(lng, lat) {
-        var lng = Number(lng);
-        var lat = Number(lat);
-        return {
-            center: [lng, lat],
-            geometry: {
-                type: "Point",
-                coordinates: [lng, lat]
-            },
-            place_name: 'Lat: ' + lat + ', Lng: ' + lng,
-            place_type: ['coordinate'],
-            properties: {},
-            type: 'Feature'
-        };
-    }
-    var coord1 = matches[1];
-    var coord2 = matches[2];
-    var geocodes = [];
-    if (coord1 < -90 || coord1 > 90) {
-        // must be lng, lat
-        geocodes.push(coordinateFeature(coord1, coord2));
-    }
-    if (coord2 < -90 || coord2 > 90) {
-        // must be lat, lng
-        geocodes.push(coordinateFeature(coord2, coord1));
-    }
-    if (geocodes.length == 0) {
-        // else could be either
-        geocodes.push(coordinateFeature(coord1, coord2));
-        geocodes.push(coordinateFeature(coord2, coord1));
-    }
-    console.log(geocodes);
-    return geocodes;
-};
-
 var geocoder = new MapboxGeocoder({
-  accessToken: window.localStorage.getItem('MapboxAccessToken'),
-  localGeocoder: function (query) {
-      return coordinatesGeocoder(query);
-  }
+  limit: 20,
+  searchTypes: ["address", "park"]
 });
 
 window.geocoder = geocoder;
 
-var button = document.createElement('button');
-button.textContent = 'click me';
+var button = document.createElement("button");
+button.textContent = "click me";
 
-var removeBtn = document.body.appendChild(document.createElement('button'));
-removeBtn.style.position = 'absolute';
+var removeBtn = document.body.appendChild(document.createElement("button"));
+removeBtn.style.position = "absolute";
 removeBtn.style.zIndex = 10;
-removeBtn.style.top = '10px';
-removeBtn.style.left = '10px';
-removeBtn.textContent = 'Remove geocoder control';
+removeBtn.style.top = "10px";
+removeBtn.style.left = "10px";
+removeBtn.textContent = "Remove geocoder control";
 
-map.getContainer().querySelector('.mapboxgl-ctrl-bottom-left').appendChild(button);
+map
+  .getContainer()
+  .querySelector(".mapboxgl-ctrl-bottom-left")
+  .appendChild(button);
 map.addControl(geocoder);
 
-map.on('load', function() {
-  button.addEventListener('click', function() {
-    geocoder.query('Montreal Quebec');
+map.on("load", function() {
+  button.addEventListener("click", function() {
+    geocoder.query("Montreal Quebec");
   });
-  removeBtn.addEventListener('click', function() {
+  removeBtn.addEventListener("click", function() {
     map.removeControl(geocoder);
-  })
+  });
 });
 
-geocoder.on('results', function(e) {
-  console.log('results: ', e.features);
+geocoder.on("results", function(e) {
+  console.log("results: ", e.features);
 });
 
-geocoder.on('error', function(e) {
-  console.log('Error is', e.error);
+geocoder.on("error", function(e) {
+  console.log("Error is", e.error);
 });
